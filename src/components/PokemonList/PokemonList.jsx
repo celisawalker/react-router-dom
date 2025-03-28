25174aac4dd9fb99943982fc4b9c080bab27068c
celisawalker <celisa.walker@gmail.com>
2025-03-28 09:37:59 -0700

    "created pokemon list component"

---
+++ src/components/PokemonList/PokemonList.jsx
@@ -0,0 +1,18 @@
+const pokemonList = (props) => {
+
+    
+    return(
+        <>
+            <h2>Pokemon</h2>
+            <ul>
+                {props.pokemon.map(currentPokemon) => (
+                    <li key={currentPokemon.name}>
+                        {currentPokemon.name}
+                    </li>
+                )}
+            </ul>
+        </>
+    )
+}
+
+export default pokemonList
